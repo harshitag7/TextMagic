6c6eef920c1df165a598684406344a153a06ccc4
harshitag7 <harshag677@gmail.com>
2022-09-24 03:34:43 +0530

    Updates

---
+++ precache-manifest.b08af8457f2d59e8fc07edb8231c687f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7246a247f112bb79deb7",
+    "url": "/text-magic/static/css/main.3222a862.chunk.css"
+  },
+  {
+    "revision": "7246a247f112bb79deb7",
+    "url": "/text-magic/static/js/main.7246a247.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/text-magic/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/text-magic/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "6301fb6989add517cb85",
+    "url": "/text-magic/static/js/runtime~main.6301fb69.js"
+  },
+  {
+    "revision": "cee77d99a8938daffc7966c6300458cf",
+    "url": "/text-magic/index.html"
+  }
+];
